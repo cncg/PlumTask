bc9e660a9479b60ebc2de07cef3308bce7fb5c0c
Chelzo <ccalvo0314@gmail.com>
2024-02-25 11:34:20 -0500

    dark mode adjustments

--- TodoList/TodoList/Views/AddView.swift
+++ TodoList/TodoList/Views/AddView.swift
@@ -22,7 +22,7 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height:55)
-                    .background(.mercury)
+                    .background(Color(UIColor.secondarySystemBackground))
                     .cornerRadius(10)
                 
                 Button(action: saveButtonPressed, label: {
@@ -64,11 +64,22 @@ struct AddView: View {
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            AddView()
+        Group {
+            NavigationView {
+                AddView()
+                
+            }
+            .preferredColorScheme(.light)
+            .environmentObject(ListViewModel())
             
+            NavigationView {
+                AddView()
+                
+            }
+            .preferredColorScheme(.dark)
+            .environmentObject(ListViewModel())
         }
-        .environmentObject(ListViewModel())
+        
         
     }
 }
--- TodoList/TodoList/Views/ListView.swift
+++ TodoList/TodoList/Views/ListView.swift
@@ -16,6 +16,7 @@ struct ListView: View {
         ZStack {
             if listViewModel.items.isEmpty {
                 NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
             } else {
                 List {
                     ForEach(listViewModel.items) { item in
